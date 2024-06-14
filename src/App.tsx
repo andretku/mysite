ddc81b6c984981af2b2f8f587cc2ac7408126cb6
Andrey_Tkachuk <andretku@gmail.com>
2024-06-14 13:19:32 +0200

    Start my site

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+
+
+
+  
+  return (
+    <div className="App">
+      <br /><br /><br />
+      <h1>Hello guys!</h1>
+      <h2>Site under construction...</h2>
+    </div>
+  );
+}
+
+export default App;
